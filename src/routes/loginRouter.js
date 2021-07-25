c3e6379f8f6003895eeec7afd52f841e65f2aed0
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-25 14:11:34 +0200

    Create path login

---
+++ src/routes/loginRouter.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const loginController = require('../controllers/loginController');
+
+router.post("/", async (req, res) => {
+    try {
+
+        const emailCheck = req.body.email;
+        const passwordCheck = req.body.password;
+
+        let token = await loginController.validate( emailCheck, passwordCheck );
+        let user = await userController.userEmail( emailCheck );
+
+        res.status(200).json( {token, user})
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+module.exports = router;
