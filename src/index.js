79a4b7ed0792ff729532e841db28278286d1387b
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-27 16:20:32 +0200

    Add new path to find the port

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.use(router);
 
 db
 .then(() => {
-    app.listen(port, () => console.log(`Node server runing on http://localhost:${port}` ));
+    // app.listen(port, () => console.log(`Node server runing on http://localhost:${port}` ));
+    app.listen(process.env.PORT || 3001, function(){
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    });
 })
 .catch((err) => console.log(err.message));
+
