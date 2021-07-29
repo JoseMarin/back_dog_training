bf08e30c97bc24e26b5bfe6f2bb1c9ca0f3b3635
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-29 10:59:18 +0200

    Change the port to 5000

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("../db");
 const app = express();
 const router = require('../router');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 //Middleware
