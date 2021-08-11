a977f7d1cd65dd85d1e5726670a08c70802f92e7
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-11 19:13:54 +0200

    Update files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const db = require("../db");
+const db = require("./db");
 const app = express();
-const router = require('../router');
+const router = require('./router');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
