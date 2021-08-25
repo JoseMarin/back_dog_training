4781d1ddc813defdf8d29f00913162c8ec7e7f30
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-25 02:54:23 +0200

    Add postId && userId

--- src/migrations/20210725155951-create-comments.js
+++ src/migrations/20210725155951-create-comments.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      postId: {
+        type: Sequelize.INTEGER,
+      },
       content: {
         type: Sequelize.STRING
       },
@@ -16,6 +19,9 @@ module.exports = {
       },
       lastName: {
         type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
       },
       date: {
         type: Sequelize.DATE
