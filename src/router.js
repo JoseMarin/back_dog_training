c5711053c48ba7a3368956ae3566652a6c94d01d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-25 02:53:35 +0200

    Add path comments

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const loginRouter = require('./routes/loginRouter');
 const userRouter = require('./routes/userRouter');
 const postRouter = require('./routes/postRouter');
+const commentsRouter = require('./routes/commentsRouter');
 
 router.use('/login', loginRouter);
 router.use('/users', userRouter);
 router.use('/post', postRouter);
+router.use('/comments', commentsRouter);
 // router.use('/admin', adminRouter);
 
 module.exports = router;
